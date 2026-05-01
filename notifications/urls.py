cb329d000913cb740c14a4ecc35e31192f31e8da
ahmed-elemam7 <ahmed.elemam.elazouni@gmail.com>
2026-05-01 15:58:35 +0300

    Update notification URLs

--- notifications/urls.py
+++ notifications/urls.py
@@ -9,6 +9,6 @@ app_name = "notifications"
 
 urlpatterns = [
     path("", NotificationListView.as_view(), name="list"),
-    path("read/", MarkNotificationAsReadView.as_view(), name="mark_as_read"),
-    path("read-all/", MarkAllNotificationsAsReadView.as_view(), name="mark_all_as_read"),
+    path("read/", MarkNotificationAsReadView.as_view(), name="mark.as.read"),
+    path("read-all/", MarkAllNotificationsAsReadView.as_view(), name="mark.all.as.read"),
 ]
