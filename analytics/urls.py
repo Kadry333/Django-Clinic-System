4fbc5c03c464f35877bff9046ae618dfec6cddd2
mawadaelmeshad <mawadahassan21@gmail.com>
2026-05-02 07:56:40 +0300

    export csv done

--- analytics/urls.py
+++ analytics/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("admin-dashboard/", views.AnalyticsDashboardView.as_view(), name="admin_dashboard"),
+    path("export/csv/", views.export_appointments_csv, name="export_appointments_csv"),
 ]
